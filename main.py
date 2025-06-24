6452168237bcd44cd0a773efcfe4520fc1e4d749
Akimitsu-Kobayashi <akimitsukobayashi07@gmail.com>
2025-06-24 15:55:11 +0800

    made a working window for the game to happen using the values in the settiings.py

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
+from settings import *
 import sys
 
 class App:
     def __init__(self):
-        pass
+        pg.init()
+        pg.display.set_caption('Tetris')
+        self.screen = pg.display.set_mode(FIELD_RES)
+        self.clock = pg.time.Clock()
+
+    def update(self):
+        self.clock.tick(FPS)
+
+    def draw(self):
+        self.screen.fill(color=FIELD_COLOR)
+        pg.display.flip()
+
+    def check_events(self):
+        if event.type == pg.QUIT or (event.type == pg.KEYDOWN and event.key ==pg.K_ESCAPE):
+            pg.quit()
+            sys.exit()
 
 if __name__ == "__main__":
     app = App()
