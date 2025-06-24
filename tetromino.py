94be41c773601a6c8190ca46314871b761b2883c
Akimitsu-Kobayashi <akimitsukobayashi07@gmail.com>
2025-06-24 17:38:34 +0800

    made the tetrominoes fall on their ownm

--- tetromino.py
+++ tetromino.py
@@ -1,5 +1,5 @@
 from settings import *
-
+import random
 class Block(pg.sprite.Sprite):
     def __init__(self, tetromino, pos):
         self.tetromino = tetromino
@@ -8,16 +8,24 @@ class Block(pg.sprite.Sprite):
         super().__init__(tetromino.tetris.sprite_group)
         self.image = pg.Surface([TILE_SIZE, TILE_SIZE])
         self.image.fill("orange")
-
         self.rect = self.image.get_rect()
+        
+    def set_rect_pos(self):
         self.rect.topleft = self.pos * TILE_SIZE
 
+    def update(self):
+        self.set_rect_pos()
 class Tetromino:
     def __init__(self, tetris):
         self.tetris = tetris
-        self.shape = '3L'
+        self.shape = random.choice(list(TETROMINOES.keys()))
         self.blocks = [Block(self, pos) for pos in TETROMINOES[self.shape]]
 
+    def move(self, direction):
+        move_direction = MOVE_DIRECTIONS[direction]
+        for block in self.blocks:
+            block.pos += move_direction
+
     def update(self):
-        pass
+        self.move(direction='down')
         
