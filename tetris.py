df98e942a74d8c4e85dc1e07db0b79f2a9bfb9e2
Akimitsu-Kobayashi <akimitsukobayashi07@gmail.com>
2025-06-24 16:28:49 +0800

    added grids

--- tetris.py
+++ tetris.py
@@ -5,8 +5,14 @@ class Tetris:
     def __init__(self, app):
         self.app = app
 
+    def draw_grid(self):
+        for x_direction in range(FIELD_W):
+            for y_direction in range(FIELD_H):
+                pg.draw.rect(self.app.screen, "black",
+                             (x_direction * TILE_SIZE, y_direction * TILE_SIZE, TILE_SIZE, TILE_SIZE), 1)
+
     def update(self):
         pass
 
     def draw(self):
-        pass
+        self.draw_grid()
